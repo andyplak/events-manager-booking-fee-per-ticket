34e64b86d039ff0b08a4bc14925f58309ffe7525
Andy Place <info@andyplace.co.uk>
2022-01-10 11:47:30 +0000

    Initial commit Add covid bond column to ticket form table and WC Cart

---
+++ events-manager-covid-bond.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Plugin Name: Events Manager Covid Bonds
+ * Plugin URI: https://github.com/andyplak/events-manager-covid-bonds
+ * Description: Addition of Covid Bonds to Events Manager Tickets
+ * Version: 1.0
+ * Author: Andy Place
+ * Author URI: http://www.andyplace.co.uk/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+function ad_ac_init() {
+    require plugin_dir_path( __FILE__ ) . 'src/class-ticket-display.php';
+    new TicketDisplay();
+
+    if( is_admin() ) {
+        require plugin_dir_path( __FILE__ ) . 'src/class-ticket-admin.php';
+        new TicketAdmin();
+    }
+}
+ad_ac_init();
---
+++ src/class-ticket-display.php
@@ -0,0 +1,56 @@
+<?php
+
+class TicketDisplay {
+
+    const COVID_BOND_PERCENTAGE = 10;
+
+    public function __construct() {
+        // Booking form
+        add_filter('em_booking_form_tickets_cols', [$this, 'em_booking_form_tickets_cols'], 10, 2);
+        add_action('em_booking_form_tickets_col_covid_bond', [$this, 'em_booking_form_tickets_col_covid_bond'], 10, 2);
+
+        // WC Cart
+        add_action( 'woocommerce_after_cart_item_name', [$this, 'woocommerce_after_cart_item_name'], 10, 2 );
+        add_filter( 'woocommerce_cart_item_price', [$this, 'woocommerce_cart_item_price'], 10, 3 );
+    }
+
+    public function em_booking_form_tickets_cols( $columns, $EM_Event ) {
+        // Check event is setup for Covid Bonds?
+        {
+            $spaces = $columns['spaces'];
+            unset( $columns['spaces'] );
+            $columns['covid_bond'] = __('Covid Bond','events-manager');
+            $columns['spaces'] = $spaces;
+        }
+        return $columns;
+    }
+
+    public function em_booking_form_tickets_col_covid_bond($EM_Ticket, $EM_Event) {
+        $price = $EM_Ticket->get_price();
+        $bond = $price / Self::COVID_BOND_PERCENTAGE;
+
+        ?>
+        <td class="em-bookings-ticket-table-covid-bond">
+            <?php echo $EM_Ticket->format_price( $bond ) ?>
+        </td>
+        <?php
+    }
+
+    public function woocommerce_after_cart_item_name( $cart_item, $cart_item_key ) {
+        // check ticket has covid bond enabled
+        {
+            echo '<div><em>Includes Non Refundbable Covid Bond</em></div>';
+        }
+    }
+
+    public function woocommerce_cart_item_price( $price, $cart_item, $cart_item_key ) {
+        // check ticket has covid bond enabled
+        {
+            $product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+            $bond    = $product->get_price() / Self::COVID_BOND_PERCENTAGE;
+            return $price . '<br /><em>'. wc_price( $bond ) .'</em>';
+        }
+        return $price;
+    }
+
+}
