248135bbd25274150a90ca892a0718813c2afed1
Andy Place <info@andyplace.co.uk>
2022-01-26 06:45:26 +0000

    Add restrictions on refund for Covid Bond line items. Apply refund step of 0.9 so that all but covid bond can be redfunded. Also restrict input on the line total and line tax input so the quantity has to be used. Not ideal but very little in the way of customisations allowed in WooCommere's html-order-item.php

--- src/class-bookings-admin.php
+++ src/class-bookings-admin.php
@@ -5,6 +5,8 @@ class BookingsAdmin {
     public function __construct() {
         add_filter( 'em_bookings_table_cols_template', [$this, 'em_bookings_table_cols_template'] );
         add_filter( 'em_bookings_table_rows_col_covid_bond_total', [$this, 'em_bookings_table_rows_col_covid_bond_total'], 20, 5);
+        add_filter( 'woocommerce_quantity_input_step_admin', [$this, 'woocommerce_quantity_input_step_admin'], 10, 3 );
+        add_action( 'admin_head', [$this, 'admin_css'], 100 );
     }
 
     public function em_bookings_table_cols_template( $cols ) {
@@ -17,6 +19,35 @@ class BookingsAdmin {
             return $EM_Booking->format_price( $EM_Booking->booking_meta['covid_bond_total'] );
         }
         return $val;
+    }
+
+    public function woocommerce_quantity_input_step_admin( $step, $product, $type ) {
+        if( $type == 'refund' ) {
+            $event = Events_Manager_WooCommerce\Product::is_event_ticket( $product );
+            if( $event ) {
+                $EM_Ticket = new EM_Ticket( $event['ticket_id'] );
+                if( $EM_Ticket ) {
+                    // Check if ticket has covid bond
+                    if( isset( $EM_Ticket->ticket_meta['covid_bond'] ) && $EM_Ticket->ticket_meta['covid_bond'] ) {
+                        return 0.9;
+                    }
+                }
+            }
+
+        }
+        return $step;
+    }
+
+    function admin_css() {
+        ?>
+        <style>
+            .refund_line_total,
+            .refund_line_tax {
+                pointer-events: none;
+                background-color: #f0f0f1 !important;
+            }
+        </style>';
+        <?php
     }
 }
 
