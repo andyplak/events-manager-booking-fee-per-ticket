68a11906a2a13f9b08a0300306654fe5461b6c9c
Andy Place <info@andyplace.co.uk>
2022-11-02 10:24:26 +0100

    WIP Handling refunds. Comment out percentage based adaptations.

--- src/class-bookings-admin.php
+++ src/class-bookings-admin.php
@@ -5,8 +5,8 @@ class BookingsAdmin {
     public function __construct() {
         add_filter( 'em_bookings_table_cols_template', [$this, 'em_bookings_table_cols_template'] );
         add_filter( 'em_bookings_table_rows_col_booking_fee_total', [$this, 'em_bookings_table_rows_col_booking_fee_total'], 20, 5);
-        add_filter( 'woocommerce_quantity_input_step_admin', [$this, 'woocommerce_quantity_input_step_admin'], 10, 3 );
-        add_action( 'admin_head', [$this, 'admin_css'], 100 );
+        #add_filter( 'woocommerce_quantity_input_step_admin', [$this, 'woocommerce_quantity_input_step_admin'], 10, 3 );
+        #add_action( 'admin_head', [$this, 'admin_css'], 100 );
     }
 
     public function em_bookings_table_cols_template( $cols ) {
@@ -21,33 +21,36 @@ class BookingsAdmin {
         return $val;
     }
 
-    public function woocommerce_quantity_input_step_admin( $step, $product, $type ) {
-        if( $type == 'refund' ) {
-            $event = Events_Manager_WooCommerce\Product::is_event_ticket( $product );
-            if( $event ) {
-                $EM_Ticket = new EM_Ticket( $event['ticket_id'] );
-                if( $EM_Ticket ) {
-                    // Check if ticket has booking fee
-                    if( isset( $EM_Ticket->ticket_meta['booking_fee'] ) && $EM_Ticket->ticket_meta['booking_fee'] > 0 ) {
-                        return $EM_Ticket->ticket_meta['booking_fee'];
-                    }
-                }
-            }
+#    public function woocommerce_quantity_input_step_admin( $step, $product, $type ) {
+#        if( $type == 'refund' ) {
+#
+#            $event = Events_Manager_WooCommerce\Product::is_event_ticket( $product );
+#            if( $event ) {
+#                $EM_Ticket = new EM_Ticket( $event['ticket_id'] );
+#                if( $EM_Ticket ) {
+#                    // Check if ticket has booking fee
+#                    if( isset( $EM_Ticket->ticket_meta['booking_fee'] ) && $EM_Ticket->ticket_meta['booking_fee'] > 0 ) {
+#                        return $EM_Ticket->ticket_meta['booking_fee'];
+#                    }
+#                }
+#            }
+#
+#        }
+#        return $step;
+#    }
 
-        }
-        return $step;
-    }
-
-    function admin_css() {
-        ?>
-        <style>
-            .refund_line_total,
-            .refund_line_tax {
-                pointer-events: none;
-                background-color: #f0f0f1 !important;
-            }
-        </style>';
-        <?php
-    }
+/*
+#    function admin_css() {
+#        ?>
+#        <style>
+#            .refund_line_total,
+#            .refund_line_tax {
+#                pointer-events: none;
+#                background-color: #f0f0f1 !important;
+#            }
+#        </style>';
+#        <?php
+#    }
+*/
 }
 
